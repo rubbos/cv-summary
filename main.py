a677246c880c64cc98a05754e03c94d30fd2b352
Ruben <41753645+rubbos@users.noreply.github.com>
2025-09-20 01:14:08 +0200

    added gemma3:4b model call to summarize text

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pdfplumber
+from ollama import chat
+from ollama import ChatResponse
 
 st.title("Summarize CV")
 st.write("Upload the CV and get a summary of the last 2 years of activities.")
@@ -9,11 +11,21 @@ if uploaded_file is not None:
     # Shows pdf
     st.pdf(uploaded_file)
 
+    # Extract text from pdf
     with pdfplumber.open(uploaded_file) as pdf:
         full_text = ""
 
         for page in pdf.pages:
             words = page.extract_text(x_tolerance=0.5)
             full_text += words + "\n\n"
+        
+        response: ChatResponse = chat(model='gemma3:4b', messages=[
+          {
+            'role': 'user',
+            'content': f'{full_text}, summarize the last 2 years of activities in a few bullet points',
+          },
+        ])
 
+    # Display extracted text
     st.text_area("Extracted Text", full_text, height=500)
+    st.text_area("Extracted Text", response.message.content, height=500)
